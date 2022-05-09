d6c0fe9af8eb1e784c9bb82e1647a0b86b6cac3e
Ersin Bakırcı <ebakircie@gmail.com>
2022-05-09 22:25:01 +0300

    ..

--- WorkEden.Application/Models/DTOs/ProductDTO/UpdateProductDTO.cs
+++ WorkEden.Application/Models/DTOs/ProductDTO/UpdateProductDTO.cs
@@ -1,4 +1,6 @@
 ﻿using EdenWorks.Application.Models.VMs.CategoryVM;
+using EdenWorks.Domain.Enums;
+using EdenWorks.Infrastructure.Utilities;
 using Microsoft.AspNetCore.Http;
 
 namespace EdenWorks.Application.Models.DTOs.ProductDTO
@@ -13,7 +15,15 @@ namespace EdenWorks.Application.Models.DTOs.ProductDTO
         //public string ImagePath {get; set;} denenmedi
         public IFormFile? UploadPath { get; set; }
 
- 
+        public DateTime UpdatedDate => DateTime.Now;
+        public string UpdatedIpAddress => CatchIP.IpAddress;
+        public string UpdatedMachineName => Environment.MachineName;
+        public Status Status => Status.Modified;
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedIpAddress { get; set; }
+        public string CreatedMachineName { get; set; }
+
         public int CategoryId { get; set; }
         public List<CategoryListVM>? Categories { get; set; }
     }
--- WorkEden.Application/Validation/FluentValidation/ProductValidator/UpdateProductValidation.cs
+++ WorkEden.Application/Validation/FluentValidation/ProductValidator/UpdateProductValidation.cs
@@ -12,7 +12,9 @@ namespace EdenWorks.Application.Validation.FluentValidation.ProductValidator
     {
         public UpdateProductValidation()
         {
-            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(2).MaximumLength(50);
+            RuleFor(x => x.Price).NotEmpty();
+            RuleFor(x => x.CategoryId).NotEmpty();
         }
     }
 }
